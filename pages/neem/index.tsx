2bbef7090a7393250f48332a06e6fb3df21076e4
j <jarvism@protonmail.com>
2023-02-06 14:31:51 -0500

    out of time

--- pages/neem/index.tsx
+++ pages/neem/index.tsx
@@ -1,80 +1,108 @@
-import { Montserrat } from "@next/font/google";
+import { Rubik } from "@next/font/google";
+import InfoCircle from "./info_circle";
+import UserCircle from "./user_circle";
 
-const montserrat = Montserrat({ subsets: ["latin"] });
+const rubik = Rubik({ subsets: ["latin"] });
 
 const style = {
-  fontFamily: montserrat.style.fontFamily,
+  fontFamily: rubik.style.fontFamily,
 };
 
-// Fetch
-// const response = await fetch("https://randomuser.me/api/");
-// const json = await response.json();
-// const person = json.results[0];
-// console.log("PERSON:", person);
+interface User {
+  covered: boolean;
+  name: {
+    first: string;
+    last: string;
+    preferred: string;
+  };
+  subscriber: boolean;
+  insurance: "Primary" | "Secondary";
+  id: string;
+}
 
 export async function getServerSideProps() {
-  // Fetch
-  const response = await fetch("https://randomuser.me/api/");
-  const json = await response.json();
-  const person = json.results[0];
-  console.log("PERSON:", person);
+  // Fake Fetch
+  const household = [
+    {
+      covered: true,
+      name: { first: "Jerome", last: "Bell", preferred: "Rome" },
+      subscriber: true,
+      Insurance: "Primary",
+      id: "",
+    },
+  ];
   return {
-    props: { person },
+    props: { household },
   };
 }
 
 interface Props {
   // any props that come into the component
-  person: {
-    gender: string;
-    name: { title: string; first: string; last: string };
-    location: {
-      street: { number: number; name: string };
-      city: string;
-      state: string;
-      country: string;
-      postcode: number;
-      coordinates: { latitude: string; longitude: string };
-      timezone: {
-        offset: string;
-        description: string;
-      };
-    };
-    email: string;
-    login: {
-      uuid: string;
-      username: string;
-      password: string;
-      salt: string;
-      md5: string;
-      sha1: string;
-      sha256: string;
-    };
-    dob: { date: string; age: number };
-    registered: { date: string; age: number };
-    phone: string;
-    cell: string;
-    id: { name: string; value: null };
-    picture: {
-      large: string;
-      medium: string;
-      thumbnail: string;
-    };
-    nat: string;
-  };
+  household: Array<User>;
 }
 
 export default function Neem(props: Props) {
-  console.log(props);
+  // Input Handler
+  const inputHandler = (event: React.ChangeEventHandler) => {
+    console.log("event:", event);
+    return;
+  };
+
+  const listItems = props.household.map((member, i) => {
+    return (
+      <div key={i.toString()} className="flex flex-wrap w-full">
+        <div className="flex items-center w-3/12">
+          <input
+            type="checkbox"
+            checked={member.covered}
+            className="bg-primary ml-8"
+            onChange={inputHandler}
+          />
+        </div>
+        <div className="flex items-center w-4/12">
+          <span className="mr-2">
+            <UserCircle />
+          </span>
+          <span className="mr-2">{member.name.first}</span>
+          <span className="mr-2">{member.name.last}</span>
+          <span className="text-lightgray">({member.name.preferred})</span>
+        </div>
+        <div>
+          <input
+            type="radio"
+            name="radio-1"
+            className="radio bg-primary"
+            checked={member.subscriber ? true : false}
+          />
+        </div>
+        <div>{member.insurance}</div>
+        <div>{member.id}</div>
+      </div>
+    );
+  });
+
   return (
     <section
-      className="flex flex-1 flex-col items-center justify-center p-4 overflow-auto"
+      className="flex flex-1 flex-col items-center p-4 overflow-auto"
       style={style}
     >
-      <h1 className="text-3xl font-semibold">Neem</h1>
-      <div className="flex w-full overflow-auto p-4">
-        <span className="mr-2">{props.person.name.first}</span>
-        <span>{props.person.name.last}</span>
+      <div className="flex flex-col w-full overflow-auto p-4">
+        <h3 className="text-xl font-semibold mb-4">Household</h3>
+        <div className="flex flex-col py-4">
+          <div className="flex items-center">
+            {/* Column headers */}
+            <div className="flex items-center w-3/12">
+              Covered
+              {/* hero icons information-circle */}
+              <InfoCircle />
+            </div>
+            <div className="w-4/12">Name</div>
+            <div>Subscriber</div>
+            <div>Insurance</div>
+            <div>ID</div>
+          </div>
+        </div>
+        {listItems}
       </div>
     </section>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
+    colors: {
+      primary: "#70C4BB",
+      purple: "#C985FF",
+      blue: "#5B8AF0",
+      orange: "#EB8F24",
+      lightgray: "#9DA7BE",
+    },
   },
   plugins: [],
-}
+};
