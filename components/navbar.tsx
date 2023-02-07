03dd3988e9aef5eb16f79b6131d1c81aafa6a5e3
j <jarvism@protonmail.com>
2023-02-07 07:53:42 -0500

    FIX: remove unused console.log

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,7 +4,6 @@ import { useRouter } from "next/router";
 
 export default function Navbar() {
   const router = useRouter();
-  console.log("ROUTER:", router);
 
   const links = [
     { display: "Home", href: "/" },
